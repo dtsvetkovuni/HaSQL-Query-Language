8dc6957f1cd02cc129526b96eaa69bbe656f914f
Dimitar Tsvetkov <ddt1u19@soton.ac.uk>
2021-04-23 01:00:17 +0100

    all fixed and done for handin

--- Interpreter.hs
+++ Interpreter.hs
@@ -7,10 +7,10 @@ import Control.Exception
 import System.IO
 import Data.List
 
-type Row = [(String,String)]
-type File = [Row]
+type Row = [(String,String)] -- contains list of variables with their values
+type File = [Row]            -- the whole csv file format structure
 
-
+-- Evaluates the full "FinalExp", first its requirements, then prints out the bound variable columns
 evalStart :: Exp -> IO String
 evalStart (FinalExp bvs rs) =do
     evaluated <- evalRequirementList rs []
@@ -28,28 +28,30 @@ evalRequirementList :: RequirementList -> File -> IO File
 evalRequirementList [] currentFile = do return currentFile
 evalRequirementList (r:rs) currentFile = do
     result <- evalRequirement r currentFile
-    evalRequirementList rs result
+    if null result
+        then return []
+        else evalRequirementList rs result
 
 -- Eval requirements 1 by 1
 evalRequirement :: Requirement -> File -> IO File
 evalRequirement (Table name clms) currentFile = do
     file <- fileReadCsv name   -- this is a list of rows
-    let vars = evalColumnList clms
-        newTable = map (zip vars) file
-    if null currentFile
-        then return newTable
-        else return (conjunctTable currentFile newTable)
+    if null file
+        then return []
+        else do let vars = evalColumnList clms
+                    newTable = map (zip vars) file
+                if null currentFile
+                then return newTable
+                else return (conjunctTable currentFile newTable)
 
--- calls to checkIf function to evaluate all of if requirements (rle) for each row 
--- calls to completeIf function to evaluate then and else (if true it evaluates rlt) (if false it evaluates rlf)
+-- Calls to checkIf function to evaluate all of if requirements (rle) for each row 
+-- Calls to completeIf function to evaluate then and else (if true it evaluates rlt) (if false it evaluates rlf)
 -- then after the evaluation the function returns the row (either full of values or empty)
 evalRequirement (IfTF rle rlt rlf) currentFile = do
-    let result = (completeIf rle rlt rlf currentFile)
-    return result
+    return (completeIf rle rlt rlf currentFile)
 
 evalRequirement (IfT rle rlt) currentFile = do
-    let result = (completeIf rle rlt [] currentFile)
-    return result
+    return (completeIf rle rlt [] currentFile)
 
 evalRequirement (AsignVarStr v1 s1) currentFile = do
     return (map (setVarStr v1 s1) currentFile)
@@ -57,13 +59,13 @@ evalRequirement (AsignVarStr v1 s1) currentFile = do
 evalRequirement (AsignVarVar v1 v2) currentFile = do
     return (map (setVarVar v1 v2) currentFile)
 
--- filter all in the currentFile ... works for 
+-- Filter all in the currentFile ... works for 
 evalRequirement req currentFile = do
     return (filter (checkRequirement req) currentFile)
 
--- do conjunction here
+-- Does conjunction on two table files
 conjunctTable ::  File -> File -> File
-conjunctTable [] file2 = []
+conjunctTable [] _ = []
 conjunctTable file1 [] = file1
 conjunctTable (row:rows) file2 = map (row ++) file2 ++ conjunctTable rows file2
 
@@ -79,13 +81,12 @@ completeIf reqEval reqTrue reqFalse (row:rows)
                transformedTrue = applyIf reqTrue row
                transformedFalse = applyIf reqFalse row
 
--- goes over all requirements in the if (requirements) and checks whether they are true or false
+-- Goes over all requirements in the if (requirements) and checks whether they are true or false
 checkIf :: [Requirement] -> Row -> Bool
 checkIf [] _ = True
 checkIf (req:rs) row = (checkRequirement req row) && checkIf rs row
 
--- goes over the if application if (requirements) applyIfTrue applyIfFalse
---checkrequrement za ko e tuka
+-- Goes over the if application if (requirements) applyIfTrue applyIfFalse
 applyIf :: [Requirement] -> Row -> Row
 applyIf [] row = row
 applyIf ((Eq v1 v2):reqs) row
@@ -112,26 +113,26 @@ applyIf ((AsignVarVar v1 v2):reqs) row = applyIf reqs (setVarVar v1 v2 row)
 applyIf ((AsignVarStr v1 v2):reqs) row = applyIf reqs (setVarStr v1 v2 row)
 applyIf _ row = error "faulty input in If statement :("
 
--- function to fetch the value of a variable
---          var
+--          var                     :Function to fetch the value of a variable
 fetchVar :: String -> Row -> String
 fetchVar varName [] = ""
 fetchVar varName ((var,val):restOfRow)
     | varName == var = val
     | otherwise = fetchVar varName restOfRow
 
---           var         
+--            var       str                :Assigns the value of the right string to the left variable
 setVarStr :: String -> String -> Row -> Row
 setVarStr _ _ [] = []
 setVarStr changeVar newVal ((var,val):rows)
     | var == changeVar = (var,removeQuotes newVal) : setVarStr changeVar newVal rows
     | otherwise = (var,val) : setVarStr changeVar newVal rows
 
---           var        var
+--           var        var                 :Assigns the value of the right variable to the left variable
 setVarVar :: String -> String -> Row -> Row
 setVarVar _ _ [] = []
 setVarVar v1 v2 row = setVarStr v1 ("\"" ++ fetchVar v2 row ++ "\"") row
 
+-- Checks if the given requirement is true or false for the given row
 checkRequirement :: Requirement -> Row -> Bool
 checkRequirement (Eq v1 v2) row = (fetchVar v1 row) == (fetchVar v2 row)
 checkRequirement (NEq v1 v2) row = (fetchVar v1 row) /= (fetchVar v2 row)
@@ -139,11 +140,13 @@ checkRequirement (EqConst v1 v2) row = (fetchVar v1 row) == removeQuotes v2
 checkRequirement (NEqConst v1 v2) row = (fetchVar v1 row) /= removeQuotes v2
 checkRequirement _ row = True
 
+-- Prepares the output File for printing
 showResult :: [String] -> File -> String
 showResult [] _ = error "There are no bound vars dummy!"
 showResult _ [] = ""
-showResult vars outputFile = concat (sort (map (showResultLine vars) outputFile))
+showResult vars outputFile = concat (sort (map (showResultRow vars) outputFile))
 
-showResultLine :: [String] -> Row -> String
-showResultLine [var] row = fetchVar var row ++ "\n"
-showResultLine (var:vars) row = fetchVar var row ++ "," ++ showResultLine vars row
+-- Used to prepare a single row for printing
+showResultRow :: [String] -> Row -> String
+showResultRow [var] row = fetchVar var row ++ "\n"
+showResultRow (var:vars) row = fetchVar var row ++ "," ++ showResultRow vars row
